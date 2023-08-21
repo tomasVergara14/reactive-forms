d43d63a0920488bc1ec7a454b773247fd43ce14f
Tom <tomasvergara272@gmail.com>
2023-08-21 03:57:47 -0300

    sidemenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path:'reactive',
+    loadChildren: ()=> import('./reactive/reactive-routing.module').then(m=>m.ReactiveRoutingModule),
+  },
+  {
+    path:'auth',
+    loadChildren: ()=> import('./auth/auth-routing.module').then(m=>m.AuthRoutingModule),
+  },
+  {
+    path:'**',
+    redirectTo:'reactive'
+  },
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AuthModule } from './auth/auth.module';
+import { ReactiveModule } from './reactive/reactive.module';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    SharedModule
+    AuthModule,
+    BrowserModule,
+    ReactiveModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
