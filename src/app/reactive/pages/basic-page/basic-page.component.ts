2153954e1015360a5484e043083fed6a9e49a8e5
Tom <tomasvergara272@gmail.com>
2023-08-22 00:43:15 -0300

    reactive forms

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+  constructor( private fb: FormBuilder){}
+
+  public myForm: FormGroup = this.fb.group({
+    name:['' , [ Validators.required, Validators.minLength(3)]],
+    price:[0, [ Validators.required, Validators.min(0)]],
+    inStorage:[0, [ Validators.required, Validators.min(0)]],
+  })
+
+  onSave(){
+    console.log(this.myForm)
+  }
 
 }
