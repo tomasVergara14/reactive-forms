f774f123c5f6b702955db436a3d9e555814ffb87
Tom <tomasvergara272@gmail.com>
2023-08-29 21:19:09 -0300

    Add formControlArray value

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -15,6 +15,8 @@ export class DynamicPageComponent {
       ['Death Stranding', Validators.required],
     ])
   })
+
+  public newFavorite: FormControl = new FormControl()
 
   constructor( 
     private fb: FormBuilder
@@ -50,6 +52,19 @@ export class DynamicPageComponent {
     return formArray.controls[index].errors && formArray.controls[index].touched
   }
 
+  onAddFavorite():void{
+    if( this.newFavorite.invalid) return;
+
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(
+      this.fb.control( newGame, Validators.required)
+    )
+    console.log(this.favoriteGames.value)
+
+    this.newFavorite.reset();
+  }
+
   onDeleteFavorite( index: number ): void {
     this.favoriteGames.removeAt(index);
   };
@@ -61,6 +76,7 @@ export class DynamicPageComponent {
     }
 
     console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([])
     this.myForm.reset();
 
   }
