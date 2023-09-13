3135fd131ea18497041140369875890d8bbc28c8
Tom <tomasvergara272@gmail.com>
2023-09-12 23:59:51 -0300

    email validator asynchronus

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -5,17 +5,38 @@ import { Observable, delay, of, tap } from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class EmailValidatorService implements AsyncValidator{
 
-    validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
+    // validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {  // Referencia
+        
+    //     const email = control.value
+    //     console.log({ email })
+
+    //     return of({
+    //         emailTaken: true
+    //     }).pipe(
+    //         delay(2000),
+    //         tap((data)=>console.log(data))
+    //     )
+    // }
+
+    validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {  // Referencia
         
         const email = control.value
         console.log({ email })
 
-        return of({
-            emailTaken: true
-        }).pipe(
-            delay(2000),
-            tap((data)=>console.log(data))
-        )
+        const httpCallObservable = new Observable<ValidationErrors | null>( (subscriber)=>{
+
+            console.log(email)
+            if( email === 'fernando@google.com' ){
+                subscriber.next({ emailTaken: true });
+                subscriber.complete();
+                return;
+            }
+
+            subscriber.next(null);
+            subscriber.complete();
+        })
+
+        return httpCallObservable;
     }
     
 }
